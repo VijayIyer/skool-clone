993e0374e47dc04c860a1d4140799dd89c1f50f7
chenyou <chenyouhuang@gmail.com>
2023-08-24 15:12:36 -0400

    Implement user schema

---
+++ models/User.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose'
+
+const UserSchema = new mongoose.Schema({
+  firstname: {
+    type: String,
+    required: [true, 'Please provude a first name for this user']
+  },
+  lastname: {
+    type: String,
+    required: [true, 'Please provude a last name for this user']
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provude a email for this user']
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provude a password for this user']
+  }
+});
+
+export default mongoose.models.User || mongoose.model('User', UserSchema)
