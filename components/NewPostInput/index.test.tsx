d4937d74c5a475adcba7265d1b5e10795260a933
Yuanda Xu <yuandaxu@yahoo.com>
2023-08-24 23:19:16 -0400

    test file initialed

---
+++ components/NewPostInput/index.test.tsx
@@ -0,0 +1,41 @@
+describe("New post component should render", () => {
+    test("It should show avatar", () => {
+        fail();
+    })
+    test("It should show a text 'Write something'", () => {
+        fail();
+    })
+    test("It should be replaced with a form after user click the component", () => {
+        fail();
+    })
+});
+
+describe("Form component should render", () => {
+    test("It should show avatar", () => {
+        fail();
+    })
+    test("It should show username and group name", () => {
+        fail();
+    })
+    test("It should have two input, one for title, one for content", () => {
+        fail();
+    })
+    test("It should allow users to upload attachment to the content", () => {
+        fail();
+    })
+    test("It should allow users to upload link to the content", () => {
+        fail();
+    })
+    test("It should allow users to upload YouTube link to the content", () => {
+        fail();
+    })
+    test("It should allow users to upload GIF to the content", () => {
+        fail();
+    })
+    test("It should have cancel button and post button in the component", () => {
+        fail();
+    })
+    test("It should allow users to select category for the component", () => {
+        fail();
+    })
+})
