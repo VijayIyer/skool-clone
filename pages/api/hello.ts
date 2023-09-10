d308f923614bcc5b69207674b4d29972a8b7b5e3
Shen <shengfeng.wu.0308@gmail.com>
2023-09-10 11:04:50 -0400

    Hot Fix: NextJS API Testing Error
    - The cause of API testing error was the mal-configuration of test environment of Jest.
    - The current solution is to separate testing of client side(Pages and components, including server rendered ones) and server side(API)
    - To run server tests, you type npm run test:server. Similarly, for client test, it is npm run test:server.
    - Also you need to declare test type when naming your test, ending with client.test.tsx or server.test.ts
    - Try to incorporate in your ticket to see how this work.

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import dbConnect from "@/lib/dbConnect";
+import { dbConnect } from "@/lib/mongoClient";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
@@ -11,6 +11,5 @@ export default async function handler(
   res: NextApiResponse<Data>
 ) {
   const result = await dbConnect();
-  console.log(result);
   res.status(200).json({ name: "John Doe" });
 }
