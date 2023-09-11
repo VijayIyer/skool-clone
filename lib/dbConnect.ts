90a5e1e47f8862cf4c160da9295a3ebf3e26fc00
chenyou <chenyouhuang@gmail.com>
2023-09-11 16:29:13 -0400

    Change prettier setting to avoid future merge conflict

--- lib/dbConnect.ts
+++ lib/dbConnect.ts
@@ -1,7 +1,7 @@
 import mongoose, { Mongoose } from "mongoose";
 
-
 const MONGODB_URI = process.env.MONGODB_URI;
+
 if (!MONGODB_URI) {
   throw new Error(
     "Please define the MONGODB_URI environment variable inside .env.local"
