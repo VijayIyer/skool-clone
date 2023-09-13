b504046bf3f5fb08180f65b3a6689c1db0c9d503
vijay <VijayIyer>
2023-09-13 09:45:36 -0400

    Changes Settings Page layout

--- pages/settings/index.tsx
+++ pages/settings/index.tsx
@@ -1,10 +1,32 @@
 import ChangePasswordForm from "@/components/ChangePassword";
-import styles from "../../styles/ChangePassword.module.css";
+import { Grid, Typography } from "@mui/material";
+import styles from "../../styles/Settings.module.css";
 
-export default function ChangePassword() {
+export default function Settings() {
   return (
-    <main className={styles.container}>
-      <ChangePasswordForm />
-    </main>
+    <Grid
+      container
+      direction={"column"}
+      spacing={2}
+      sx={{
+        backgroundColor: "#f8f7f5",
+        height: "100vh",
+      }}
+    >
+      {/* Header bar */}
+      <Grid item xs={12}></Grid>
+      <Grid container className={styles.settingsPageContentContainer}>
+        <Grid item xs={3}>
+          {/* The left sided navigation bar inside settings page */}
+          <aside className={styles.navigationPanel}></aside>
+        </Grid>
+        <Grid item xs={9}>
+          <main className={styles.settingsPageContent}>
+            {/* Chosen component is based on option selected in navigation bar on the left */}
+            <ChangePasswordForm />
+          </main>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 }
