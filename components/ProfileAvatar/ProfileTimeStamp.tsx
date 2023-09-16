b0c215ddf29ae98f44ae7dd9e81fc88a570514ad
chenyou <chenyouhuang@gmail.com>
2023-09-15 22:59:02 -0400

    Implement the timstamp

--- components/ProfileAvatar/ProfileTimeStamp.tsx
+++ components/ProfileAvatar/ProfileTimeStamp.tsx
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useMemo } from "react";
 import QueryBuilderIcon from "@mui/icons-material/QueryBuilder";
 import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
 import { Typography } from "@mui/material";
+import moment from "moment";
 
 export default function ProfileTimeStamp({ user }) {
+  const activeTime = moment(user.activeTime).fromNow();
+  const joinedTime = moment(user.createdTime).format("MMM D,YYYY");
+
   return (
     <section>
       <div>
         <QueryBuilderIcon />
-        <Typography>{user.activeTime}</Typography>
+        <Typography>{activeTime}</Typography>
       </div>
       <div>
         <CalendarTodayIcon />
-        <Typography>{user.createdTime}</Typography>
+        <Typography>Joined {joinedTime}</Typography>
       </div>
     </section>
   );
