1012af092b64d5512ea34aa36381c42e075a45ab
Will Zhang <zhiyuan.zhang2000@outlook.com>
2023-09-20 18:09:24 -0500

    refactor unit test for login to use Mongoose, define util functions in lib, modify test cases

--- lib/userLib/hashPassword.ts
+++ lib/userLib/hashPassword.ts
@@ -1,9 +1,19 @@
-// import { findUserByEmail } from './userApi';
+import { findUserByEmail } from "./userApi";
 
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
 export async function generateHashPassword(password: string) {
   const salt = await bcrypt.genSalt();
   const hashedPassword = await bcrypt.hash(password, salt);
   return hashedPassword;
+}
+
+export async function compareHashPasswordWith(email: string, password: string) {
+  const user = await findUserByEmail(email);
+  if (user) {
+    const savedPassword = user.password;
+    return await bcrypt.compare(password, savedPassword);
+  } else {
+    return false;
+  }
 }
