477d4c130063a125db734f5f8bad52e6546251e5
vijay <VijayIyer>
2023-09-23 14:46:49 -0400

    Adds passwordChangedAt meta field with new pre hook to record password changed timestamp

--- models/User.ts
+++ models/User.ts
@@ -1,7 +1,8 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import next from "next/types";
 
 function capitalize(name: string) {
-  if (typeof name !== 'string') name = '';
+  if (typeof name !== "string") name = "";
   return name.charAt(0).toUpperCase() + name.substring(1);
 }
 
@@ -9,24 +10,29 @@ const UserSchema = new mongoose.Schema(
   {
     firstName: {
       type: String,
-      required: [true, 'Please provide a first name for this user'],
+      required: [true, "Please provide a first name for this user"],
       set: capitalize,
     },
     lastName: {
       type: String,
-      required: [true, 'Please provide a last name for this user'],
+      required: [true, "Please provide a last name for this user"],
       set: capitalize,
     },
     email: {
       type: String,
-      required: [true, 'Please provide a email for this user'],
+      required: [true, "Please provide a email for this user"],
       unique: true,
       trim: true,
       lowercase: true,
     },
     password: {
       type: String,
-      required: [true, 'Please provide a password for this user'],
+      required: [true, "Please provide a password for this user"],
+    },
+    passwordChangedAt: {
+      type: Date,
+      default: Date.now(),
+      required: true,
     },
   },
   {
@@ -34,11 +40,27 @@ const UserSchema = new mongoose.Schema(
     virtuals: {
       fullName: {
         get() {
-          return this.firstName + ' ' + this.lastName;
+          return this.firstName + " " + this.lastName;
         },
       },
     },
   }
 );
+UserSchema.pre("findOneAndUpdate", function (next) {
+  try {
+    const schema = this.getUpdate();
+    console.log(`updating passwordChangedAt field - ${JSON.stringify(schema)}`);
+    if (!schema) return next();
 
-export default mongoose.models.User || mongoose.model('User', UserSchema);
+    if (Object.hasOwn(schema, "password")) {
+      schema.passwordChangedAt = schema.$set.updatedAt;
+      return next();
+    }
+    console.log(`setting passwordChangedAt`);
+    return next();
+  } catch (err) {
+    console.error(err);
+    return next();
+  }
+});
+export default mongoose.models.User || mongoose.model("User", UserSchema);
