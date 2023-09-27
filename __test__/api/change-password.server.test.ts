d0fbb757fc785d27b62422a3d25379d86a06ff8d
vijay <VijayIyer>
2023-09-27 09:09:19 -0400

    Initialize server tests for backend code

--- __test__/api/change-password.server.test.ts
+++ __test__/api/change-password.server.test.ts
@@ -1,9 +1,9 @@
 const NoImplementationError = () => {
   throw Error("test not implemented!");
 };
-it("should return 401 Unauthorized when no jwt token is sent in the request", () =>
+it("should return 401 UnAuthorized error when no jwt token is sent in the request", () =>
   NoImplementationError());
-it("should return 401 Unauthroized when invalid credentials, like wrong email or old password are sent", () =>
+it("should return 401 UnAuthorized error when invalid credentials, like wrong email or old password are sent", () =>
   NoImplementationError());
 it("should make a call to modify user's saved password", () =>
   NoImplementationError());
