26fe4c468b02278d52ea04043d879d7be99d6085
vijay <VijayIyer>
2023-10-05 16:48:56 -0400

    Modifies OTP model to include timestamp

--- models/Otp.ts
+++ models/Otp.ts
@@ -2,20 +2,23 @@ import mongoose from "mongoose";
 
 import { Schema } from "mongoose";
 import next from "next/types";
-const OtpSchema = new Schema({
-  otp: {
-    type: Number,
-    required: true,
+const OtpSchema = new Schema(
+  {
+    otp: {
+      type: Number,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    verified: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-  },
-  verified: {
-    type: Boolean,
-    required: true,
-    default: false,
-  },
-});
+  { timestamps: true }
+);
 
-export default mongoose.models.Otp || mongoose.model("Otp", OtpSchema);
+export default mongoose.models.OTP || mongoose.model("OTP", OtpSchema);
