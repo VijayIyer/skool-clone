9b141c9a4a47bdd5a5ee2b0c71be40d07cfa08f5
vijay <VijayIyer>
2023-10-08 15:40:51 -0400

    Adds initial tests for server side utility methods

---
+++ __test__/lib/otpLib/sendOtpEmail.server.test.ts
@@ -0,0 +1,8 @@
+describe("sendOtpEmail", () => {
+  it("should call the method to send email with otp in the content to intended recipient", () => {
+    throw new Error("test not implemented");
+  });
+  it("should indicate that method failed if there is any error while sending email", () => {
+    throw new Error("test not implemented");
+  });
+});
--- __test__/lib/userLib/validation.server.test.ts
+++ __test__/lib/userLib/validation.server.test.ts
@@ -3,123 +3,126 @@ import {
   validateEmail,
   validatePassword,
   validateName,
-} from '../../../lib/userLib';
+} from "../../../lib/userLib";
 
-describe('validateUserSignUpInput', () => {
-  it('should return success when input is valid', () => {
+describe("validateUserSignUpInput", () => {
+  it("should return success when input is valid", () => {
     const input = {
-      firstName: 'John',
-      lastName: 'Doe',
-      email: 'john@example.com',
-      password: 'password123',
+      firstName: "John",
+      lastName: "Doe",
+      email: "john@example.com",
+      password: "password123",
     };
     const result = validateUserSignUpInput(input);
     expect(result).toEqual({ success: true });
   });
 
-  it('should return an error message when first name is invalid', () => {
+  it("should return an error message when first name is invalid", () => {
     const input = {
-      firstName: '123', // Invalid first name
-      lastName: 'Doe',
-      email: 'john@example.com',
-      password: 'password123',
+      firstName: "123", // Invalid first name
+      lastName: "Doe",
+      email: "john@example.com",
+      password: "password123",
     };
     const result = validateUserSignUpInput(input);
     expect(result).toEqual({
       success: false,
       message:
-        'Validation error: First Name must be only an alphabetical string and length should be less or equal 20 charachters',
+        "Validation error: First Name must be only an alphabetical string and length should be less or equal 20 charachters",
     });
   });
 
-  it('should return an error message when last name is invalid', () => {
+  it("should return an error message when last name is invalid", () => {
     const input = {
-      firstName: 'John',
-      lastName: '123', // Invalid last name
-      email: 'john@example.com',
-      password: 'password123',
+      firstName: "John",
+      lastName: "123", // Invalid last name
+      email: "john@example.com",
+      password: "password123",
     };
     const result = validateUserSignUpInput(input);
     expect(result).toEqual({
       success: false,
       message:
-        'Validation error: Last Name must be only an alphabetical string and length should be less or equal 20 charachters',
+        "Validation error: Last Name must be only an alphabetical string and length should be less or equal 20 charachters",
     });
   });
 
-  it('should return an error message when email is invalid', () => {
+  it("should return an error message when email is invalid", () => {
     const input = {
-      firstName: 'John',
-      lastName: 'Doe',
-      email: 'invalid-email', // Invalid email
-      password: 'password123',
+      firstName: "John",
+      lastName: "Doe",
+      email: "invalid-email", // Invalid email
+      password: "password123",
     };
     const result = validateUserSignUpInput(input);
     expect(result).toEqual({
       success: false,
-      message: 'Validation error: Invalid email address format',
+      message: "Validation error: Invalid email address format",
     });
   });
 
-  it('should return an error message when password is too short', () => {
+  it("should return an error message when password is too short", () => {
     const input = {
-      firstName: 'John',
-      lastName: 'Doe',
-      email: 'john@example.com',
-      password: '1234', // Password too short
+      firstName: "John",
+      lastName: "Doe",
+      email: "john@example.com",
+      password: "1234", // Password too short
     };
     const result = validateUserSignUpInput(input);
     expect(result).toEqual({
       success: false,
-      message: 'Validation error: Password must be at least 5 characters',
+      message: "Validation error: Password must be at least 5 characters",
     });
   });
 });
 
-describe('validateEmail', () => {
-  it('should return true for a valid email', () => {
-    const validEmail = 'john@example.com';
+describe("validateEmail", () => {
+  it("should return true for a valid email", () => {
+    const validEmail = "john@example.com";
     const result = validateEmail(validEmail);
     expect(result).toBe(true);
   });
 
-  it('should return false for an invalid email', () => {
-    const invalidEmail = 'invalid-email';
+  it("should return false for an invalid email", () => {
+    const invalidEmail = "invalid-email";
     const result = validateEmail(invalidEmail);
     expect(result).toBe(false);
   });
 });
 
-describe('validatePassword', () => {
-  it('should return true for a password with at least 5 characters', () => {
-    const validPassword = 'password123';
+describe("validatePassword", () => {
+  it("should return true for a password with at least 5 characters", () => {
+    const validPassword = "password123";
     const result = validatePassword(validPassword);
     expect(result).toBe(true);
   });
 
-  it('should return false for a password with fewer than 5 characters', () => {
-    const invalidPassword = '1234';
+  it("should return false for a password with fewer than 5 characters", () => {
+    const invalidPassword = "1234";
     const result = validatePassword(invalidPassword);
     expect(result).toBe(false);
   });
 });
 
-describe('validateName', () => {
-  it('should return false for a name with space', () => {
-    const validName = 'John Doe';
+describe("validateName", () => {
+  it("should return false for a name with space", () => {
+    const validName = "John Doe";
     const result = validateName(validName);
     expect(result).toBe(false);
   });
 
-  it('should return true for a valid single-word name', () => {
-    const validName = 'John';
+  it("should return true for a valid single-word name", () => {
+    const validName = "John";
     const result = validateName(validName);
     expect(result).toBe(true);
   });
 
-  it('should return false for a name with non-alphabetical characters', () => {
-    const invalidName = 'John123';
+  it("should return false for a name with non-alphabetical characters", () => {
+    const invalidName = "John123";
     const result = validateName(invalidName);
     expect(result).toBe(false);
   });
+});
+describe("validateGenerateOtpInput", () => {
+  expect(true).toBe(true);
 });
