8ee1a9ff8c4ea36a28ac35b85b019f05d91805b2
vijay <VijayIyer>
2023-10-10 10:50:36 -0400

    Deletes unncessary client side test case file

--- __test__/pages/signup.client.test.tsx
+++
@@ -1,9 +0,0 @@
-const NotImplemented = () => {
-  throw new Error("Test Not Implemented!");
-};
-describe("signUpPage", () => {
-  it("should display signUpVerificationForm on signup submission", () => {
-    NotImplemented();
-  });
-  it("should display signUpForm by default", () => NotImplemented());
-});
